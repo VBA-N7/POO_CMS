59bac41ba110b0788c5917a397e25604b0968fd0
Esquerre <gregoire.esquerre@gmail.com>
2020-11-17 00:10:13 +0100

    class moteur added

---
+++ moteur.cpp
@@ -0,0 +1,6 @@
+#include "moteur.h"
+
+float moteur::get_puissance(void)
+{
+    return puissance;
+}
--- moteur.h
+++ moteur.h
@@ -1,1 +1,21 @@
-#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class moteur
+{
+protected:
+    float cylindree;
+    string boite;
+    float puissance;
+    float consommation;
+
+float K;
+float Q=0.055;
+
+public:
+    virtual void calcul_consommation(void)=0;
+    virtual void calcul_puissance(void)=0;
+    float get_puissance(void);
+};
