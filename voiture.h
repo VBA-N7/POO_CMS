9244366f7d25835e60c9327eed9db2fc613b9db2
VBA-N7 <barthe.vincent@hotmail.fr>
2020-11-20 18:37:37 +0100

    Adding price attribute and getter for voiture class.

--- voiture.h
+++ voiture.h
@@ -1,14 +1,18 @@
 #include "chassis.h"
 #include "moteur.h"
 
+#define STARTING_PRICE 15e3
+
 class Voiture
 {
 public:
 	Voiture();
 	//void calcul_vitesse(void);
+	float get_prix(void);
 
 	Chassis* CH;
 	//moteur* MT;
+	float prix = STARTING_PRICE;	
 protected:
 	float vitesse_max;
 };
